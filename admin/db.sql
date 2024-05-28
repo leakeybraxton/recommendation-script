5b48db1cf1a0b8433ab6992a90f64f427cd5d20d
hartmund <hartmund.wendlandt@outlook.com>
2024-05-28 08:23:37 +0200

    New base sql

--- admin/db.sql
+++ admin/db.sql
@@ -1,60 +1,77 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Adminer 4.8.1 MySQL 10.11.7-MariaDB-cll-lve dump
 
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+
+SET NAMES utf8mb4;
+
+DROP DATABASE IF EXISTS `u956006469_rr_leads`;
+CREATE DATABASE `u956006469_rr_leads` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `u956006469_rr_leads`;
+
+DROP TABLE IF EXISTS `leads`;
 CREATE TABLE `leads` (
-  `id` int(22) NOT NULL,
+  `id` int(22) NOT NULL AUTO_INCREMENT,
   `imported_time` int(11) DEFAULT NULL,
-  `picked_up` varchar(255) DEFAULT NULL,
-  `pitched` varchar(255) DEFAULT NULL,
-  `call_end_result` varchar(255) DEFAULT NULL,
-  `call_history` longtext DEFAULT NULL,
-  `appointment_setter` varchar(255) DEFAULT NULL,
+  `picked_up` text DEFAULT NULL,
+  `pitched` text DEFAULT NULL,
+  `call_end_result` text DEFAULT NULL,
+  `call_history` text DEFAULT NULL,
+  `appointment_setter` text DEFAULT NULL,
   `locked_status` int(1) DEFAULT NULL,
   `queue` int(22) DEFAULT NULL,
-  `company_name` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `website` varchar(255) DEFAULT NULL,
-  `phone_number` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `google_rating` varchar(255) DEFAULT NULL,
-  `google_reviews` varchar(255) DEFAULT NULL,
-  `yelp_rating` varchar(255) DEFAULT NULL,
-  `yelp_reviews` varchar(255) DEFAULT NULL,
-  `fb_rating` varchar(255) DEFAULT NULL,
-  `fb_reviews` varchar(255) DEFAULT NULL,
-  `fb_likes` varchar(255) DEFAULT NULL,
-  `fb_checkins` varchar(255) DEFAULT NULL,
-  `fb_followers` varchar(255) DEFAULT NULL,
-  `is_title` varchar(255) DEFAULT NULL,
-  `is_description` varchar(255) DEFAULT NULL,
-  `is_adwords` varchar(255) DEFAULT NULL,
-  `is_facebook_ads` varchar(255) DEFAULT NULL,
-  `is_twitter_ads` varchar(255) DEFAULT NULL,
-  `is_linkedin_ads` varchar(255) DEFAULT NULL,
-  `is_bing_ads` varchar(255) DEFAULT NULL,
-  `is_robot` varchar(255) DEFAULT NULL,
-  `is_ssl` varchar(255) DEFAULT NULL,
-  `is_wordpress` varchar(255) DEFAULT NULL,
-  `fb_url` varchar(255) DEFAULT NULL,
-  `twitter_url` varchar(255) DEFAULT NULL,
-  `linkedin_url` varchar(255) DEFAULT NULL,
-  `youtube_url` varchar(255) DEFAULT NULL,
-  `instagram_url` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `search_type` varchar(255) DEFAULT NULL,
-  `crawler_status` varchar(255) DEFAULT NULL,
-  `founded` varchar(255) DEFAULT NULL,
-  `company_type` varchar(255) DEFAULT NULL,
-  `company_size` varchar(255) DEFAULT NULL
+  `company_name` text DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `website` text DEFAULT NULL,
+  `phone_number` text DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `google_rating` text DEFAULT NULL,
+  `google_reviews` text DEFAULT NULL,
+  `yelp_rating` text DEFAULT NULL,
+  `yelp_reviews` text DEFAULT NULL,
+  `fb_rating` text DEFAULT NULL,
+  `fb_reviews` text DEFAULT NULL,
+  `fb_likes` text DEFAULT NULL,
+  `fb_checkins` text DEFAULT NULL,
+  `fb_followers` text DEFAULT NULL,
+  `is_title` text DEFAULT NULL,
+  `is_description` text DEFAULT NULL,
+  `is_adwords` text DEFAULT NULL,
+  `is_facebook_ads` text DEFAULT NULL,
+  `is_twitter_ads` text DEFAULT NULL,
+  `is_linkedin_ads` text DEFAULT NULL,
+  `is_bing_ads` text DEFAULT NULL,
+  `is_robot` text DEFAULT NULL,
+  `is_ssl` text DEFAULT NULL,
+  `is_wordpress` text DEFAULT NULL,
+  `fb_url` text DEFAULT NULL,
+  `twitter_url` text DEFAULT NULL,
+  `linkedin_url` text DEFAULT NULL,
+  `youtube_url` text DEFAULT NULL,
+  `instagram_url` text DEFAULT NULL,
+  `category` text DEFAULT NULL,
+  `search_type` text DEFAULT NULL,
+  `crawler_status` text DEFAULT NULL,
+  `founded` text DEFAULT NULL,
+  `company_type` text DEFAULT NULL,
+  `company_size` text DEFAULT NULL,
+  `date_time_utc` text DEFAULT NULL,
+  `city` text DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `robot_config` text DEFAULT NULL,
+  `bbb_profile` text DEFAULT NULL,
+  `state` text DEFAULT NULL,
+  `years_in_business` text DEFAULT NULL,
+  `first_name` text DEFAULT NULL,
+  `last_name` text DEFAULT NULL,
+  `contact_job_title` text DEFAULT NULL,
+  `revenue` text DEFAULT NULL,
+  `top_keywords` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `company_name` (`company_name`,`address`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-ALTER TABLE `leads`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `company_name` (`company_name`,`address`);
-
-
-ALTER TABLE `leads`
-  MODIFY `id` int(22) NOT NULL AUTO_INCREMENT;
-COMMIT;
+-- 2024-05-28 06:15:04
