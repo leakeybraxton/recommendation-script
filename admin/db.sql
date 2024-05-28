820e71d45b6b035bf2dae51b54e1cfbcb59ef28e
hartmund <hartmund.wendlandt@outlook.com>
2024-05-28 17:09:24 +0200

    Updated hidden column

--- admin/db.sql
+++ admin/db.sql
@@ -3,14 +3,14 @@ SET foreign_key_checks = 0;
 SET NAMES utf8mb4;
 
 DROP DATABASE IF EXISTS `u956006469_rr_leads`;
-CREATE DATABASE `u956006469_rr_leads` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE `u956006469_rr_leads`;
 USE `u956006469_rr_leads`;
 
 DROP TABLE IF EXISTS `leads`;
 CREATE TABLE `leads` (
   `id` int(22) NOT NULL AUTO_INCREMENT,
   `imported_time` int(11) DEFAULT NULL,
-  `hidden` int(1) DEFAULT NULL,
+  `hidden`  tinyint(1) unsigned NOT NULL DEFAULT 0,
   `picked_up` text DEFAULT NULL,
   `pitched` text DEFAULT NULL,
   `call_end_result` text DEFAULT NULL,
